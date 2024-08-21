9bb75ff4637f0aa5476c4e37e456dcaaa6a140b1
Chun Wei Chong <35936417-ccwei0929@users.noreply.replit.com>
2024-08-21 15:44:32 +0000

    Add comment into the code

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-
+# Function to load the data from the csv file
 def read_csv(file_name):
     df = pd.read_csv(file_name)
     return df
 
-
+# To get the value from the table
 def get_val(val):
     table_1_data = read_csv('table1.csv')
     table_1_data.set_index('Index #', inplace=True)
     value = table_1_data.loc[val, 'Value']
     return value
 
-
+# To count the exact value of the table
 def get_table_2_val():
 
     alpha = int(get_val('A5') + get_val('A20'))
@@ -27,7 +27,7 @@ def get_table_2_val():
 
     return table_2_data
 
-
+# Design part, print the title and the description
 st.title("Assignment (Mulah Tech)")
 table1 = read_csv('table1.csv')
 st.table(table1)
